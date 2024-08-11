94983ae43dacc57c09406d2bf04369822503d4c1
avy-gagneja <63822995+avy-gagneja@users.noreply.github.com>
2024-08-11 16:07:22 -0700

    Update weatherscript.js

--- build/weatherscript.js
+++ build/weatherscript.js
@@ -189,13 +189,13 @@ function updateBackground(condition, hours) {
     }
 
     if (condition.includes('clear')) {
-        backgroundImage = isDay ? 'url(assets/sun-bg.jpeg)' : 'url(assets/night-bg.png)';
+        backgroundImage = isDay ? 'url(assets/sun-bg.jpeg)' : 'url(assets/night-bg2.png)';
     } else if (condition.includes('clouds')) {
         backgroundImage = isDay ? 'url(assets/day-cloud-bg.jpg)' : 'url(assets/night-cloud-bg.jpeg)';
     } else if (condition.includes('rain')) {
         backgroundImage = isDay ? 'url(assets/day-rain-bg.jpg)' : 'url(assets/night-rain-bg.jpeg)';
     } else {
-        backgroundImage = isDay ? 'url(assets/sun-bg.jpeg)' : 'url(assets/night-bg.png)';
+        backgroundImage = isDay ? 'url(assets/sun-bg.jpeg)' : 'url(assets/night-bg2.png)';
     }
 
     document.body.style.backgroundImage = backgroundImage;
